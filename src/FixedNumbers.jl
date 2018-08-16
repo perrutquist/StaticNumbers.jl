79d3675c73f55205aa5888e94f6bda3d070bc4b6
Per Rutquist <per.rutquist@gmail.com>
2018-08-16 16:09:34 +0200

    More constructors. Promote methods (untested)

--- src/FixedNumbers.jl
+++ src/FixedNumbers.jl
@@ -16,7 +16,9 @@ struct FixedInteger{X} <: Integer
 end
 
 FixedInteger(x::Integer) = FixedInteger{x}()
-Base.convert(x::Type{FixedInteger{X}}, y::Number) where {X} = X == y ? x : InexactError(:convert, FixedInteger{X}(), y)
+FixedInteger(x::Bool) = FixedInteger{Int(x)}() # Use FixedInteger{false}() if you must...
+Base.convert(::Type{FixedInteger{X}}, y::Number) where {X} = FixedInteger{X}() == y ? x : InexactError(:convert, FixedInteger{X}(), y)
+Base.convert(::Type{T}, ::FixedInteger{X}) where {T<:Number,X} = convert(T, X)
 
 """
 A `FixedReal` is a `Real` whose value is stored in the type, and which
@@ -30,21 +32,23 @@ struct FixedReal{X} <: Real
 end
 
 FixedReal(x::Real) = FixedReal{x}()
-Base.convert(x::Type{FixedReal{X}}, y::Number) where {X} = X == y ? x : InexactError(:convert, FixedReal{X}(), y)
+Base.convert(x::Type{FixedReal{X}}, y::Number) where {X} = X == y ? FixedReal{X}() : InexactError(:convert, FixedReal{X}(), y)
+Base.convert(::Type{T}, ::FixedReal{X}) where {T<:Number,X} = convert(T, X)
 
 """
 A `FixedNumber` is a `Number` whose value is stored in the type, and which
 contains no runtime data.
 """
 struct FixedNumber{X} <: Number
-    function FixedInteger{X}() where {X}
+    function FixedNumber{X}() where {X}
         X isa Number && isimmutable(X) || throw(FixedError)
         new{X}()
     end
 end
 
 FixedNumber(x::Number) = FixedNumber{x}()
-Base.convert(x::Type{FixedNumber{X}}, y::Number) where {X} = X == y ? x : InexactError(:convert, FixedNumber{X}(), y)
+Base.convert(x::Type{FixedNumber{X}}, y::Number) where {X} = X == y ? FixedNumber{X}() : InexactError(:convert, FixedNumber{X}(), y)
+Base.convert(::Type{T}, ::FixedNumber{X}) where {T<:Number,X} = convert(T, X)
 
 """
 `Fixed` is short-hand for the `Union` of all `FixedInteger`s, `FixedReal`s and
@@ -60,5 +64,23 @@ function Fixed(x::Number)
     round(x) != x && return FixedReal(x)
     return FixedInteger(Integer(x))
 end
+
+Base.promote_rule(::Type{<:FixedInteger}, ::Type{<:FixedInteger}) = Int
+Base.promote_rule(::Type{<:FixedReal}, ::Type{<:Union{FixedReal,FixedInteger}}) = Float64
+Base.promote_rule(::Type{<:FixedNumber}, ::Type{<:Fixed}) = Complex{Float64}
+
+Base.promote_rule(::Type{<:Fixed}, ::Type{T}) where {T<:Number} = T
+
+# Some of the more common constructors that do not default to `convert`
+for T in (:Bool, :Int32, :UInt32, :Int64, :UInt64, :Int128)
+    @eval Base.$T(::FixedInteger{X}) where X = $T(X)
+end
+for T in (:Float32, :Float64)
+    @eval Base.$T(::Union{FixedInteger{X}, FixedReal{X}}) where X = $T(X)
+end
+for T in (:ComplexF32, :ComplexF64)
+    @eval Base.$T(::Union{FixedInteger{X}, FixedReal{X}, FixedNumber{X}}) where X = $T(X)
+end
+# big(x) still defaults to convert.
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,15 @@
 using FixedNumbers
 using Test
 
-@test Fixed(1.0) == FixedInteger{1}()
+@test Fixed(1.0) === FixedInteger{1}()
+@test Fixed(1+0im) === FixedInteger{1}()
+
+@test Fixed(1.5) === FixedReal{1.5}()
+@test Fixed(1+im) === FixedNumber{1+im}()
+
+@test Float64(Fixed(1.0)) === 1.0
+
+@test big(Fixed(1)) == big(1)
+@test typeof(big(Fixed(1))) == BigInt
+
+@test Complex{Float64}(Fixed(2)) === ComplexF64(2)
