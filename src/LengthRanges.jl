fbc2c657381d0dd00a15215c7ee3fec045492a3e
Per Rutquist <per.rutquist@gmail.com>
2020-01-19 19:26:33 +0100

    More aggressive inlining

--- src/LengthRanges.jl
+++ src/LengthRanges.jl
@@ -259,5 +259,5 @@ Takes 0-based linear index and a variable number of ranges.
 end
 
 @inline function Base.Tuple(g::Base.Generator{<:Base.Iterators.ProductIterator{<:Tuple{Vararg{<:StaticLengthRange}}}})
-    ntuple(i -> g.f(Base.unsafe_getindex(g.iter, i)), static(length(g)))
+    ntuple(i -> (Base.@_inline_meta; g.f(Base.unsafe_getindex(g.iter, i))), static(length(g)))
 end
