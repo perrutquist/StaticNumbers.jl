930935ec1449287bc95de06bcaa57cef3b1327e3
Per Rutquist <per.rutquist@gmail.com>
2020-01-27 14:32:48 +0100

    WIP: docs with Documenter.jl

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter
+using StaticNumbers
+
+makedocs(
+    sitename = "StaticNumbers",
+    format = Documenter.HTML(),
+    modules = [StaticNumbers],
+    pages = [
+        "index.md"
+        "reference.md"
+        "example.md"
+    ]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
---
+++ docs/src/reference.md
@@ -0,0 +1,19 @@
+# Functions
+
+```@meta
+CurrentModule = StaticNumbers
+```
+
+```@docs
+Static
+static
+unstatic
+StaticBool
+StaticInteger
+StaticReal
+StaticNumber
+StaticOrInt
+StaticOrBool
+@staticnumbers
+@generate_static_methods
+```
